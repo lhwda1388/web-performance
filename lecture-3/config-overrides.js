b8d8ea47d1f6ac16b023b810d0ca4b63ce2f2cdb
LEE HYUN WOO <lhwda1388@naver.com>
2022-01-21 17:06:00 +0900

    font preload

---
+++ lecture-3/config-overrides.js
@@ -0,0 +1,15 @@
+const PreloadWebpackPlugin = require("preload-webpack-plugin");
+
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+
+  config.plugins.push(
+    new PreloadWebpackPlugin({
+      rel: "preload",
+      as: "font",
+      include: "allAssets",
+      fileWhitelist: [/(.woff2?)/i],
+    })
+  );
+  return config;
+};
